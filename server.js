6c8e398bb5972800b4b8840777eaa65777cc1b9f
Viacheslav Kondratiuk <viacheslav.kondratiuk@trustyou.net>
2015-11-02 09:57:54 +0200

    Trying to make node.js server work

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
  * Created by vkondratyuk on 02.11.15.
  */
 
-var app = require('express')();
-app.use(express.static('public'));
-app.listen(process.env.PORT);
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
